1e1c8b6650c6b7cfb118c8d828d8a9e7c5b77ede
igorStolyarov <terema13@gmail.com>
2016-10-26 15:39:32 +0300

    7

---
+++ src/ua/com/goit/gojavafive/groupeight/igorstolyarov/module01/Gsdx.java
@@ -0,0 +1,7 @@
+package ua.com.goit.gojavafive.groupeight.igorstolyarov.module01;
+
+public class Gsdx {
+    public static void main(String[] args) {
+        System.out.println("sdfsdf");
+    }
+}
