f7affc217c05ab73e7890e6d7deb2a42a2fa2121
igorStolyarov <terema13@gmail.com>
2016-10-26 15:44:56 +0300

    9

---
+++ src/ua/com/goit/gojavafive/groupeight/igorstolyarov/module02/Dsa.java
@@ -0,0 +1,10 @@
+package ua.com.goit.gojavafive.groupeight.igorstolyarov.module02;
+
+/**
+ * Created by Igor on 26.10.2016.
+ */
+public class Dsa {
+    public static void main(String[] args) {
+        System.out.println("sdfsdfsd");
+    }
+}
