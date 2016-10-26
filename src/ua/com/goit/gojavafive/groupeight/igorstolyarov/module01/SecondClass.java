296e9f967c5e74fc1783ac19292dec071fa39fa8
igorStolyarov <terema13@gmail.com>
2016-10-26 13:17:21 +0300

    second commit

---
+++ src/ua/com/goit/gojavafive/groupeight/igorstolyarov/module01/SecondClass.java
@@ -0,0 +1,7 @@
+package ua.com.goit.gojavafive.groupeight.igorstolyarov.module01;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
