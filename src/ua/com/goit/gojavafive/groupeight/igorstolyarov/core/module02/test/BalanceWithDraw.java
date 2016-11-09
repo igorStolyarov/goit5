1c7944cb9664f9aee45ae5fb9bc98ed41ea6aa08
igorStolyarov <terema13@gmail.com>
2016-11-09 14:41:42 +0200

    test 01

---
+++ src/ua/com/goit/gojavafive/groupeight/igorstolyarov/core/module02/test/BalanceWithDraw.java
@@ -0,0 +1,14 @@
+package ua.com.goit.gojavafive.groupeight.igorstolyarov.core.module02.test;
+
+public class BalanceWithDraw {
+
+    public static int bool(int balance, int withdraw) {
+        if (withdraw <= balance) {
+            System.out.println("OK. " + "Your balance : " + (balance - withdraw));
+
+        } else {
+            System.out.println("NO. " + "Your balance : " + (balance));
+        }
+        return balance;
+    }
+}
